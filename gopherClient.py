76a484580adc57ccdfc3cdcb00318fb183475b23
Mateusz Myalski <mateusz.myalski@gmail.com>
2019-02-18 10:08:56 +0100

    test

---
+++ gopherClient.py
@@ -0,0 +1,21 @@
+import socket
+import os
+
+clear = lambda: os.system('cls')
+
+def printBuffer(request):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("gopher.floodgap.com", 70))
+    s.send(request.encode("UTF-8"))
+    buffer = s.recv(1024)
+    while len(buffer) > 0:
+        print(buffer.decode("UTF-8"))
+        buffer = s.recv(1024)
+        
+
+
+printBuffer("\r\n")
+
+printBuffer("new\r\n")
+
+
